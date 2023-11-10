682c68027542e18030758e5d9a3ad8b490c1a075
Mahmoud-Git123 <abdelfa2@vdi-scclab-039.lancs.local>
2023-11-10 06:09:35 +0000

    add mine detection if that displays image if there is a bomb in the square

--- Resources/BombSquare.java
+++ Resources/BombSquare.java
@@ -1,19 +1,34 @@
 import java.util.*;
 
+import javax.swing.ImageIcon;
+
 public class BombSquare extends GameSquare
 {
+
+
 	private boolean thisSquareHasBomb = false;
 	public static final int MINE_PROBABILITY = 10;
-
 	public BombSquare(int x, int y, GameBoard board)
 	{
-		super(x, y, "images/blank.png", board);
+		super(x, y, "Resources/images/blank.png", board);
 
 		Random r = new Random();
 		thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
 	}
 
+	
+	GameSquare currentSquare;
+
 	public void clicked()
 	{
+		
+			if (thisSquareHasBomb){
+				setImage("Resources/images/bomb.png");
+
+			} 
 	}
+
+
+
+
 }
